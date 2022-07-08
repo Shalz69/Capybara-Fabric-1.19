a19eb78319ce38fb191025d6e7c7b54c48ec070d
Shalz <hollowshalz@gmail.com>
2022-07-08 15:25:56 +0200

    Initial commit

---
+++ src/main/java/net/shalz/capybara/Capybara.java
@@ -0,0 +1,15 @@
+package net.shalz.capybara;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Capybara implements ModInitializer {
+	public static final String MOD_ID = "capybara";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shalz/capybara/CapybaraClient.java
@@ -0,0 +1,10 @@
+package net.shalz.capybara;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CapybaraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
